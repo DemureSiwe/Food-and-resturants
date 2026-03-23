b1851ccc5f4206eb5b97f3e0866914726d539546
DemureBusi <letsetse.b@gmail.com>
2026-03-23 03:06:39 +0200

    added responsiveness to the website

--- scripts.js
+++ scripts.js
@@ -3,3 +3,16 @@ const header = document.querySelector("header");
 window.addEventListener("scroll", function () {
   header.classList.toggle("sticky", window.scrollY > 80);
 });
+
+let menu = document.querySelector("#menu-icon");
+let navlist = document.querySelector(".navlist");
+
+menu.onclick = () => {
+  menu.classList.toggle("fa-solid fa-bars");
+  navlist.classList.toggle("open");
+};
+
+window.onscroll = () => {
+  menu.classList.remove("fa-solid fa-bars");
+  navlist.classList.remove("open");
+};
